acfd3f3a56e3205d4f679d78a73f1ad23744ce83
Tempest <623059008@qq.com>
2020-05-06 19:17:59 +0800

    first commit

---
+++ src/controller/index.js
@@ -0,0 +1,40 @@
+const Base = require('./base.js');
+
+module.exports = class extends Base {
+  indexAction() {
+    return this.display();
+  }
+  hw3Action() {
+    return this.display();
+  }
+  hw4Action() {
+    return this.display();
+  }
+  hw5Action() {
+    return this.display();
+  }
+  hw6Action() {
+    return this.display();
+  }
+  hw7Action() {
+    return this.display();
+  }
+  hw8Action() {
+    return this.display();
+  }
+  quizAction() {
+    return this.display();
+  }
+  gameAction() {
+    return this.display();
+  }
+  interactiveCubeAction() {
+    return this.display();
+  }
+  textAction() {
+    return this.display();
+  }
+  reflectionAction() {
+    return this.display();
+  }
+};
